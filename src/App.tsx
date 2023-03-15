d57d91fc7ed5cc6166dbfaa7b0b300ea41d391ef
leeroygbiv <youtheart@gmail.com>
2023-03-15 21:51:37 +0700

    Try removing all TWA styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { AI } from "./components/AI";
 import { Counter } from "./components/Counter";
 import { Jetton } from "./components/Jetton";
 import { TransferTon } from "./components/TransferTon";
-import styled from "styled-components";
 import { Button, FlexBoxCol, FlexBoxRow } from "./components/styled/styled";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { CHAIN } from "@tonconnect/protocol";
@@ -12,16 +11,6 @@ import "@twa-dev/sdk";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-
-const StyledApp = styled.div`
-  min-height: 100vh;
-  padding: 20px 20px;
-`;
-
-const AppContainer = styled.div`
-  max-width: 900px;
-  margin: 0 auto;
-`;
 
 const darkTheme = createTheme({
   palette: {
@@ -33,31 +22,27 @@ function App() {
   const { network } = useTonConnect();
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <StyledApp>
-        <AppContainer>
-          <FlexBoxCol>
-            <h1>MONETON by BL</h1>
-            <Box>Test using Box.</Box>
-            <FlexBoxRow>
-              <TonConnectButton />
-              <Button>
-                {network
-                  ? network === CHAIN.MAINNET
-                    ? "mainnet"
-                    : "testnet"
-                  : "N/A"}
-              </Button>
-            </FlexBoxRow>
-            <AI />
-            <Counter />
-            <TransferTon />
-            <Jetton />
-          </FlexBoxCol>
-        </AppContainer>
-      </StyledApp>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <h1>MONETON by BL</h1>
+        <Box>Test v 1.</Box>
+        <FlexBoxRow>
+          <TonConnectButton />
+          <Button>
+            {network
+              ? network === CHAIN.MAINNET
+                ? "mainnet"
+                : "testnet"
+              : "N/A"}
+          </Button>
+        </FlexBoxRow>
+        <AI />
+        <Counter />
+        <TransferTon />
+        <Jetton />
+      </ThemeProvider>
+    </>
   );
 }
 
--- src/components/AI.tsx
+++ src/components/AI.tsx
@@ -228,120 +228,116 @@ export function AI() {
   });
 
   return (
-    <div className="Container">
-      <Card>
-        <FlexBoxCol>
-          <h2>txt2img</h2>
-          <Box sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, gap: { xs: 2, md: 4 }, width: '100%', mb: 2 }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%' }}>
-              <MUITextField
-                id="prompt"
-                display="Prompt"
-                allValues={allValues}
-                // updateAllValues={allValues => setAllValues(allValues)}
-                setAllValues={setAllValues}
-              />
-              <MUITextField
-                id="negative_prompt"
-                display="Negative prompt"
-                allValues={allValues}
-                // updateAllValues={allValues => setAllValues(allValues)}
-                setAllValues={setAllValues}
-              />
-            </Box>
-            <Box sx={{ width: { xs: '100%', md: '37.5%' } }}>
-              <Button
-                fullWidth
-                size={'large'}
-                sx={{ height: { xs: '3.5rem', md: '100%' }, fontSize: { xs: '1.125rem', md: '1.25rem' } }}
-                onClick={() => mockApi(allValues)}
-                variant="contained"
-              >
-                Imagine
-              </Button>
-            </Box>
-          </Box>
-          <Box sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, gap: { xs: 2, md: 4 }, width: '100%' }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: { xs: '100%', md: '50%'} }}>
-              <MUISlider
-                id="steps"
-                display="Sampling steps"
-                min={1}
-                max={150}
-                step={1}
-                allValues={allValues}
-                setAllValues={setAllValues}
-              />
-              <MUIRadio
-                id="sampler_index"
-                display="Sampling method"
-                controls={
-                  [
-                    'Euler a',
-                    'Euler',
-                    'LMS',
-                    'Heun'
-                  ]
-                }
-                allValues={allValues}
-                setAllValues={setAllValues}
-              />
-              <MUISlider
-                id="width"
-                display="Width"
-                min={64}
-                max={512}
-                step={64}
-                allValues={allValues}
-                setAllValues={setAllValues}
-              />
-              <MUISlider
-                id="height"
-                display="Height"
-                min={64}
-                max={512}
-                step={64}
-                allValues={allValues}
-                setAllValues={setAllValues}
-              />
-              <MUISlider
-                id="batch_count"
-                display="Batch count"
-                min={1}
-                max={100}
-                step={1}
-                allValues={allValues}
-                setAllValues={setAllValues}
-              />
-              <MUISlider
-                id="cfg_scale"
-                display="CFG scale"
-                min={1}
-                max={30}
-                step={.5}
-                allValues={allValues}
-                setAllValues={setAllValues}
-              />
-              <MUINumber
-                id="seed"
-                display="Seed"
-                min={-1}
-                max={9999999999}
-                step={1}
-                allValues={allValues}
-                setAllValues={setAllValues}
-              />
-            </Box>
-            <Box sx={{ width: { xs: '100%', md: '50%'} }}>
-              <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <Box sx={{ position: 'relative', width: '100%', paddingTop: '100%', background: 'rgba(255, 255, 255, .5)' }}>
-                  <Box component="img" id="a" sx={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', objectFit: 'contain' }} src={"data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="} width="512" height="512" alt="" />
-                </Box>
-              </Box>
+    <>
+      <h2>txt2img</h2>
+      <Box sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, gap: { xs: 2, md: 4 }, width: '100%', mb: 2 }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%' }}>
+          <MUITextField
+            id="prompt"
+            display="Prompt"
+            allValues={allValues}
+            // updateAllValues={allValues => setAllValues(allValues)}
+            setAllValues={setAllValues}
+          />
+          <MUITextField
+            id="negative_prompt"
+            display="Negative prompt"
+            allValues={allValues}
+            // updateAllValues={allValues => setAllValues(allValues)}
+            setAllValues={setAllValues}
+          />
+        </Box>
+        <Box sx={{ width: { xs: '100%', md: '37.5%' } }}>
+          <Button
+            fullWidth
+            size={'large'}
+            sx={{ height: { xs: '3.5rem', md: '100%' }, fontSize: { xs: '1.125rem', md: '1.25rem' } }}
+            onClick={() => mockApi(allValues)}
+            variant="contained"
+          >
+            Imagine
+          </Button>
+        </Box>
+      </Box>
+      <Box sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, gap: { xs: 2, md: 4 }, width: '100%' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: { xs: '100%', md: '50%'} }}>
+          <MUISlider
+            id="steps"
+            display="Sampling steps"
+            min={1}
+            max={150}
+            step={1}
+            allValues={allValues}
+            setAllValues={setAllValues}
+          />
+          <MUIRadio
+            id="sampler_index"
+            display="Sampling method"
+            controls={
+              [
+                'Euler a',
+                'Euler',
+                'LMS',
+                'Heun'
+              ]
+            }
+            allValues={allValues}
+            setAllValues={setAllValues}
+          />
+          <MUISlider
+            id="width"
+            display="Width"
+            min={64}
+            max={512}
+            step={64}
+            allValues={allValues}
+            setAllValues={setAllValues}
+          />
+          <MUISlider
+            id="height"
+            display="Height"
+            min={64}
+            max={512}
+            step={64}
+            allValues={allValues}
+            setAllValues={setAllValues}
+          />
+          <MUISlider
+            id="batch_count"
+            display="Batch count"
+            min={1}
+            max={100}
+            step={1}
+            allValues={allValues}
+            setAllValues={setAllValues}
+          />
+          <MUISlider
+            id="cfg_scale"
+            display="CFG scale"
+            min={1}
+            max={30}
+            step={.5}
+            allValues={allValues}
+            setAllValues={setAllValues}
+          />
+          <MUINumber
+            id="seed"
+            display="Seed"
+            min={-1}
+            max={9999999999}
+            step={1}
+            allValues={allValues}
+            setAllValues={setAllValues}
+          />
+        </Box>
+        <Box sx={{ width: { xs: '100%', md: '50%'} }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Box sx={{ position: 'relative', width: '100%', paddingTop: '100%', background: 'rgba(255, 255, 255, .5)' }}>
+              <Box component="img" id="a" sx={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', objectFit: 'contain' }} src={"data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="} width="512" height="512" alt="" />
             </Box>
           </Box>
-        </FlexBoxCol>
-      </Card>
-    </div>
+        </Box>
+      </Box>
+    </>
   );
 }
